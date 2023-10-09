1670c5d9ecc87eb90cea325fadd0191b6313e368
B00150644 <B00150644@mytudublin.ie>
2023-10-09 23:34:46 +0100

    unit3

---
+++ Unit3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstascle"))
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle")) 
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        } 
+
+    }
+}
